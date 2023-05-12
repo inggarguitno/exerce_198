320e33f14bcafa5ff72c5fd09fba7c37e213f812
inggarguitno <127089742+inggarguitno@users.noreply.github.com>
2023-05-12 10:46:39 +0700

    Add files via upload

---
+++ exe _search_198.cpp
@@ -0,0 +1,31 @@
+
+
+#include <iostream>
+using namespace std;
+
+float inggar(float a, float t) {
+	return 98 * a * t;
+}
+float guritno(float r) {
+	return 12 * r * r;
+}
+switch (pilihan) {
+	cout << "lowerbound" << endl;
+	cout << "upperbound" << endl;
+	cin >> set mid;
+	cout << "inggar" < 10 << endl;
+	cin >> found;
+	cout << "inggar" > 10 << endl;
+	cin >> not found;
+
+}
+
+int main() {
+	int n, arr(98), lowerbound, upperbound, mid = 0;
+	cout << "98"
+		cin >> n;
+	cout << "inggar";
+	cin >> n;
+	cout << "adi"; 
+	cin >> n;
+}
